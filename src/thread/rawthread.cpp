d4064f251366ae3dff7a411e176077eb8ae69cbf
James W. McClain <jmcclain@azavea.com>
2019-02-14 11:41:17 -0500

    Timings

--- src/thread/rawthread.cpp
+++ src/thread/rawthread.cpp
@@ -27,6 +27,7 @@
 
 #include <boost/random/mersenne_twister.hpp>
 #include <boost/random/uniform_int_distribution.hpp>
+#include <boost/timer/timer.hpp>
 
 #include "gdal.h"
 #include "gdal_utils.h"
@@ -68,6 +69,9 @@ char temp_path[PATH_LEN];
 // Randomness
 namespace r = boost::random;
 
+// Timer
+namespace t = boost::timer;
+
 // Expected
 auto expected = std::vector<std::size_t>();
 auto hash = std::hash<std::string>();
@@ -75,13 +79,13 @@ auto hash = std::hash<std::string>();
 // ANSI
 // Reference: https://stackoverflow.com/questions/3219393/stdlib-and-colored-output-in-c
 // Reference: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
-#define ANSI_COLOR_RED     "\x1b[31;1m"
-#define ANSI_COLOR_GREEN   "\x1b[32;1m"
-#define ANSI_COLOR_YELLOW  "\x1b[33;1m"
-#define ANSI_COLOR_BLUE    "\x1b[34;1m"
+#define ANSI_COLOR_RED "\x1b[31;1m"
+#define ANSI_COLOR_GREEN "\x1b[32;1m"
+#define ANSI_COLOR_YELLOW "\x1b[33;1m"
+#define ANSI_COLOR_BLUE "\x1b[34;1m"
 #define ANSI_COLOR_MAGENTA "\x1b[35;1m"
-#define ANSI_COLOR_CYAN    "\x1b[36;1m"
-#define ANSI_COLOR_RESET   "\x1b[0m"
+#define ANSI_COLOR_CYAN "\x1b[36;1m"
+#define ANSI_COLOR_RESET "\x1b[0m"
 
 void *reader(void *)
 {
@@ -137,61 +141,79 @@ int main(int argc, char **argv)
     app_options = GDALWarpAppOptionsNew(options, nullptr);
     source = GDALOpen(argv[1], GA_ReadOnly);
     sprintf(temp_path, temp_template, getpid(), 0);
-    dataset = GDALWarp(temp_path, nullptr, 1, &source, app_options, 0);
-    band = GDALGetRasterBand(dataset, 1);
+    {
+        t::auto_cpu_timer timer;
+
+        fprintf(stdout, ANSI_COLOR_GREEN "DATASET\n" ANSI_COLOR_RESET);
+        dataset = GDALWarp(temp_path, nullptr, 1, &source, app_options, 0);
+        band = GDALGetRasterBand(dataset, 1);
+    }
     width = GDALGetRasterXSize(dataset);
     height = GDALGetRasterYSize(dataset);
     x = (width / WINDOW_SIZE) - 1;
     y = (height / WINDOW_SIZE) - 1;
 
     // Expected
-    fprintf(stdout, ANSI_COLOR_GREEN "Computing expected results\n" ANSI_COLOR_RESET);
     expected.resize(x * y);
-    for (int i = 0; i < x; ++i)
     {
-        for (int j = 0; j < y; ++j)
+        t::auto_cpu_timer timer;
+
+        fprintf(stdout, ANSI_COLOR_GREEN "EXPECTED RESULTS\n" ANSI_COLOR_RESET);
+        for (int i = 0; i < x; ++i)
         {
-            uint8_t buffer[TILE_SIZE * TILE_SIZE + 1];
-            buffer[TILE_SIZE * TILE_SIZE] = 0;
-            auto retval = GDALRasterIO(
-                band,                             // source band
-                GF_Read,                          // mode
-                i * WINDOW_SIZE, j * WINDOW_SIZE, // read offsets
-                WINDOW_SIZE, WINDOW_SIZE,         // read width, height
-                buffer,                           // write buffer
-                TILE_SIZE, TILE_SIZE,             // write width, height
-                GDT_Byte,                         // destination type
-                0, 0                              // stride
-            );
-
-            if (retval != CE_None)
+            for (int j = 0; j < y; ++j)
             {
-                exit(-1);
-            }
+                uint8_t buffer[TILE_SIZE * TILE_SIZE + 1];
+                buffer[TILE_SIZE * TILE_SIZE] = 0;
+                auto retval = GDALRasterIO(
+                    band,                             // source band
+                    GF_Read,                          // mode
+                    i * WINDOW_SIZE, j * WINDOW_SIZE, // read offsets
+                    WINDOW_SIZE, WINDOW_SIZE,         // read width, height
+                    buffer,                           // write buffer
+                    TILE_SIZE, TILE_SIZE,             // write width, height
+                    GDT_Byte,                         // destination type
+                    0, 0                              // stride
+                );
 
-            auto s = std::string(reinterpret_cast<char *>(buffer));
-            expected[i + j * x] = hash(s);
+                if (retval != CE_None)
+                {
+                    exit(-1);
+                }
+
+                auto s = std::string(reinterpret_cast<char *>(buffer));
+                expected[i + j * x] = hash(s);
+            }
         }
     }
 
     GDALClose(dataset);
     unlink(temp_path);
     sprintf(temp_path, temp_template, getpid(), 1);
-    dataset = GDALWarp(temp_path, nullptr, 1, &source, app_options, 0);
-    band = GDALGetRasterBand(dataset, 1);
+    {
+        t::auto_cpu_timer timer;
+
+        fprintf(stdout, ANSI_COLOR_GREEN "SECOND DATASET\n" ANSI_COLOR_RESET);
+        dataset = GDALWarp(temp_path, nullptr, 1, &source, app_options, 0);
+        band = GDALGetRasterBand(dataset, 1);
+    }
 
     // Reuse one dataset
-    fprintf(stdout, ANSI_COLOR_GREEN "Checking results\n" ANSI_COLOR_RESET);
-    for (int i = 0; i < N; ++i)
     {
-        assert(pthread_create(&threads[i], nullptr, reader, 0) == 0);
+        t::auto_cpu_timer timer;
+
+        fprintf(stdout, ANSI_COLOR_GREEN "ACTUAL RESULTS\n" ANSI_COLOR_RESET);
+        for (int i = 0; i < N; ++i)
+        {
+            assert(pthread_create(&threads[i], nullptr, reader, 0) == 0);
+        }
+        for (int i = 0; i < N; ++i)
+        {
+            assert(pthread_join(threads[i], nullptr) == 0);
+            fprintf(stdout, ANSI_COLOR_MAGENTA "." ANSI_COLOR_RESET);
+        }
+        fprintf(stdout, "\n");
     }
-    for (int i = 0; i < N; ++i)
-    {
-        assert(pthread_join(threads[i], nullptr) == 0);
-        fprintf(stdout, ANSI_COLOR_MAGENTA "." ANSI_COLOR_RESET);
-    }
-    fprintf(stdout, "\n");
 
     // Cleanup
     GDALWarpAppOptionsFree(app_options);
