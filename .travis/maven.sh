e24423a73c98e7faf43fe85e91e04dcd9a704f0c
James W. McClain <jmcclain@azavea.com>
2019-03-06 15:02:09 -0500

    Tweak Maven Name

--- .travis/maven.sh
+++ .travis/maven.sh
@@ -5,7 +5,7 @@ cp /tmp/.travis/settings.xml ~/.m2/
 mvn deploy:deploy-file \
   -DgroupId="com.azavea.gdal" \
   -DartifactId="gdal-warp-bindings" \
-  -Dversion="33-$COMMIT" \
+  -Dversion="33.$COMMIT" \
   -Dpackaging="jar" \
   -Dfile="/tmp/src/main/gdalwarp.jar" \
   -DrepositoryId="bintray" \
