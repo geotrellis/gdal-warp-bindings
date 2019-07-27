fcfe40987b76866c91cc755b9fbdfd6d76353720
James W. McClain <jmcclain@azavea.com>
2019-07-26 20:34:34 -0400

    Update Metadata Test Program

--- src/main/GDALWarpMetadataTest.java
+++ src/main/GDALWarpMetadataTest.java
@@ -32,25 +32,92 @@ class GDALWarpMetadataTest {
         long token = GDALWarp.get_token(args[0], options);
         long before, after;
         int len = 0;
-        //byte[][] domain_list1 = new byte[1 << 10][1 << 10];
 
-        // Domain List Metadata
-        before = System.currentTimeMillis();
-        for (int j = 0; j < (1 << 18); ++j) {
-            byte[][] domain_list1 = new byte[1 << 4][1 << 10];
-            GDALWarp.get_metadata_domain_list(token, GDALWarp.SOURCE, 0, 0, domain_list1);
-            for (int i = 0; i < domain_list1.length; ++i) {
-                String str = new String(domain_list1[i], "UTF-8").trim();
-                if (str.length() > 0) {
-                    len = len + str.length();
-                } else {
-                    break;
+        // Noop
+        {
+            len = 0;
+            before = System.currentTimeMillis();
+            for (int j = 0; j < (1 << 18); ++j) {
+                len += GDALWarp.noop(token, GDALWarp.SOURCE, 0);
+            }
+            after = System.currentTimeMillis();
+            System.out.println(after - before + " " + len);
+        }
+
+        // Domain List Metadata ThreadLocal
+        {
+            len = 0;
+            before = System.currentTimeMillis();
+            String[][] domain_list = new String[1][0];
+            for (int j = 0; j < (1 << 18); ++j) {
+                GDALWarp.get_metadata_domain_list(token, GDALWarp.SOURCE, 0, 0, domain_list);
+                for (int i = 0; i < domain_list[0].length; ++i) {
+                    len = len + domain_list[0][i].length();
                 }
             }
+            after = System.currentTimeMillis();
+            System.out.println(after - before + " " + len);
         }
-        after = System.currentTimeMillis();
-        System.out.println(after - before);
 
+        // Domain List Metadata Reallocate 16⨯1024
+        {
+            // Domain List Metadata
+            before = System.currentTimeMillis();
+            for (int j = 0; j < (1 << 18); ++j) {
+                byte[][] domain_list1 = new byte[1 << 4][1 << 10];
+                GDALWarp.get_metadata_domain_list(token, GDALWarp.SOURCE, 0, 0, domain_list1);
+                for (int i = 0; i < domain_list1.length; ++i) {
+                    String str = new String(domain_list1[i], "UTF-8").trim();
+                    if (str.length() > 0) {
+                        len = len + str.length();
+                    } else {
+                        break;
+                    }
+                }
+            }
+            after = System.currentTimeMillis();
+            System.out.println(after - before + " " + len);
+        }
+
+        // Domain List Metadata Reallocate 1024⨯1024
+        {
+            // Domain List Metadata
+            before = System.currentTimeMillis();
+            for (int j = 0; j < (1 << 18); ++j) {
+                byte[][] domain_list1 = new byte[1 << 10][1 << 10];
+                GDALWarp.get_metadata_domain_list(token, GDALWarp.SOURCE, 0, 0, domain_list1);
+                for (int i = 0; i < domain_list1.length; ++i) {
+                    String str = new String(domain_list1[i], "UTF-8").trim();
+                    if (str.length() > 0) {
+                        len = len + str.length();
+                    } else {
+                        break;
+                    }
+                }
+            }
+            after = System.currentTimeMillis();
+            System.out.println(after - before + " " + len);
+        }
+
+        // Domain List Metadata Preallocate
+        {
+            // Domain List Metadata
+            byte[][] domain_list1 = new byte[1 << 10][1 << 10];
+            before = System.currentTimeMillis();
+            for (int j = 0; j < (1 << 18); ++j) {
+                GDALWarp.get_metadata_domain_list(token, GDALWarp.SOURCE, 0, 0, domain_list1);
+                for (int i = 0; i < domain_list1.length; ++i) {
+                    String str = new String(domain_list1[i], "UTF-8").trim();
+                    if (str.length() > 0) {
+                        len = len + str.length();
+                    } else {
+                        break;
+                    }
+                }
+            }
+            after = System.currentTimeMillis();
+            System.out.println(after - before + " " + len);
+        }
 
         GDALWarp.deinit();
         return;
