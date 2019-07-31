4a88d580896659437f40904cf8d61efe1dc8094a
James W. McClain <jmcclain@azavea.com>
2019-07-31 07:29:02 -0400

    Print Error Codes and Severities

--- src/errorcodes.cpp
+++ src/errorcodes.cpp
@@ -40,24 +40,100 @@ static pthread_mutex_t errno_cache_lock = PTHREAD_MUTEX_INITIALIZER;
 #define CALL pthread_self()
 #endif
 
+// Reference: https://en.wikipedia.org/wiki/ANSI_escape_code#Colors
+#define ANSI_COLOR_RED "\x1b[31;1m"
+#define ANSI_COLOR_GREEN "\x1b[32;1m"
+#define ANSI_COLOR_YELLOW "\x1b[33;1m"
+#define ANSI_COLOR_BLUE "\x1b[34;1m"
+#define ANSI_COLOR_CYAN "\x1b[36;1m"
+#define ANSI_COLOR_BGYELLOW "\x1b[103;1m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+const char *severity_string(CPLErr eErrClass)
+{
+  switch (eErrClass)
+  {
+  case CE_None:
+    return ANSI_COLOR_GREEN "NON-ERROR(0)";
+  case CE_Debug:
+    return ANSI_COLOR_CYAN "DEBUG(1)";
+  case CE_Warning:
+    return ANSI_COLOR_YELLOW "WARNING(2)";
+  case CE_Failure:
+    return ANSI_COLOR_RED "FAILURE(3)";
+  case CE_Fatal:
+    return ANSI_COLOR_RED ANSI_COLOR_BGYELLOW "UNRECOVERABLE(4)";
+  default:
+    return ANSI_COLOR_BLUE "UNCLASSIFIED";
+  }
+}
+
+const char *error_string(int err_no)
+{
+  switch (err_no)
+  {
+  case CPLE_None:
+    return "CPLE_None(0) \"No error.\"";
+  case CPLE_AppDefined:
+    return "CPLE_AppDefined(1) \"Application defined error.\"";
+  case CPLE_OutOfMemory:
+    return "CPLE_OutOfMemory(2) \"Out of memory error.\"";
+  case CPLE_FileIO:
+    return "CPLE_FileIO(3) \"File I/O error.\"";
+  case CPLE_OpenFailed:
+    return "CPLE_OpenFailed(4) \"Open failed.\"";
+  case CPLE_IllegalArg:
+    return "CPLE_IllegalArg(5) \"Illegal argument.\"";
+  case CPLE_NotSupported:
+    return "CPLE_NotSupported(6) \"Not supported.\"";
+  case CPLE_AssertionFailed:
+    return "CPLE_AssertionFailed(7) \"Assertion failed.\"";
+  case CPLE_NoWriteAccess:
+    return "CPLE_NoWriteAccess(8) \"No write access.\"";
+  case CPLE_UserInterrupt:
+    return "CPLE_UserInterrupt(9) \"User interrupted\"";
+  case CPLE_ObjectNull:
+    return "CPLE_ObjectNull(10) \"NULL object.\"";
+  case CPLE_HttpResponse:
+    return "CPLE_HttpResponse(11) \"HTTP response.\"";
+  case CPLE_AWSBucketNotFound:
+    return "CPLE_AWSBucketNotFound(12) \"AWSBucketNotFound.\"";
+  case CPLE_AWSObjectNotFound:
+    return "CPLE_AWSObjectNotFound(13) \"AWSObjectNotFound.\"";
+  case CPLE_AWSAccessDenied:
+    return "CPLE_AWSAccessDenied(14) \"AWSAccessDenied.\"";
+  case CPLE_AWSInvalidCredentials:
+    return "CPLE_AWSInvalidCredentials(15) \"AWSInvalidCredentials.\"";
+  case CPLE_AWSSignatureDoesNotMatch:
+    return "CPLE_AWSSignatureDoesNotMatch(16) \"AWSSignatureDoesNotMatch.\"";
+#if defined(CPLE_AWSError)
+  case CPLE_AWSError:
+    return "CPLE_AWSError(17) \"VSIE_AWSError.\"";
+#endif
+  default:
+    return "Unknown error...";
+  }
+}
+
 /**
  * Put the most recently-encountered err_no into the cache.
  */
 void put_last_errno(CPLErr eErrClass, int err_no, const char *msg)
 {
 
-    fprintf(stderr, "%s\n", msg);
-    if (eErrClass == CE_Fatal)
-    {
-        exit(-1);
-    }
-    else
-    {
-        errno_key_t tid = CALL;
-        pthread_mutex_lock(&errno_cache_lock);
-        errno_cache->operator[](tid) = err_no;
-        pthread_mutex_unlock(&errno_cache_lock);
-    }
+  fprintf(stderr, "%s %s %s" ANSI_COLOR_RESET "\n",
+          severity_string(eErrClass), error_string(err_no), msg);
+  if (eErrClass == CE_Fatal)
+  {
+    exit(-1);
+  }
+  else
+  {
+    errno_key_t tid = CALL;
+    pthread_mutex_lock(&errno_cache_lock);
+    errno_cache->operator[](tid) = err_no;
+    pthread_mutex_unlock(&errno_cache_lock);
+  }
 }
 
 /**
@@ -65,27 +141,27 @@ void put_last_errno(CPLErr eErrClass, int err_no, const char *msg)
  */
 int get_last_errno()
 {
-    errno_key_t tid = CALL;
+  errno_key_t tid = CALL;
 
-    pthread_mutex_lock(&errno_cache_lock);
-    int retval;
-    if (errno_cache->count(tid) > 0)
-    {
-        retval = errno_cache->at(tid);
-        errno_cache->erase(tid);
-    }
-    else
-    {
-        retval = CPLE_None;
-    }
-    if (errno_cache->size() > (1 << 20))
-    {
-        // XXX Can contain errors from 2**20 unique threads before
-        // possibly losing information
-        errno_cache->clear();
-    }
-    pthread_mutex_unlock(&errno_cache_lock);
-    return retval;
+  pthread_mutex_lock(&errno_cache_lock);
+  int retval;
+  if (errno_cache->count(tid) > 0)
+  {
+    retval = errno_cache->at(tid);
+    errno_cache->erase(tid);
+  }
+  else
+  {
+    retval = CPLE_None;
+  }
+  if (errno_cache->size() > (1 << 20))
+  {
+    // XXX Can contain errors from 2**20 unique threads before
+    // possibly losing information
+    errno_cache->clear();
+  }
+  pthread_mutex_unlock(&errno_cache_lock);
+  return retval;
 }
 
 /**
@@ -93,12 +169,12 @@ int get_last_errno()
  */
 void errno_init()
 {
-    errno_cache = new errno_cache_t();
-    if (errno_cache == nullptr)
-    {
-        throw std::bad_alloc();
-    }
-    CPLSetErrorHandler(put_last_errno);
+  errno_cache = new errno_cache_t();
+  if (errno_cache == nullptr)
+  {
+    throw std::bad_alloc();
+  }
+  CPLSetErrorHandler(put_last_errno);
 }
 
 /**
@@ -106,11 +182,11 @@ void errno_init()
  */
 void errno_deinit()
 {
-    pthread_mutex_lock(&errno_cache_lock);
-    if (errno_cache != nullptr)
-    {
-        delete errno_cache;
-        errno_cache = nullptr;
-    }
-    pthread_mutex_unlock(&errno_cache_lock);
+  pthread_mutex_lock(&errno_cache_lock);
+  if (errno_cache != nullptr)
+  {
+    delete errno_cache;
+    errno_cache = nullptr;
+  }
+  pthread_mutex_unlock(&errno_cache_lock);
 }
