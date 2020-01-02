99d47ec7a48a4b4095103f0b25c42c16c46d75c0
James W. McClain <jmcclain@azavea.com>
2020-01-02 18:17:30 -0500

    Use Updated Build Environment

--- .travis/tests.sh
+++ .travis/tests.sh
@@ -5,15 +5,16 @@ ln -s /tmp/c41078a1.tif src/experiments/data/c41078a1.tif # sic
 docker run -it --rm \
       -v $(pwd):/workdir \
       -e CC=gcc -e CXX=g++ \
-      -e CFLAGS="-Wall -Werror -O0 -ggdb3 -DSO_FINI -D_GNU_SOURCE" \
+      -e CFLAGS="-Wall -Wno-sign-compare -Werror -O0 -ggdb3 -DSO_FINI -D_GNU_SOURCE" \
+      -e BOOST_ROOT="/usr/local/include/boost_1_69_0" \
       -e JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" \
-      jamesmcclain/gdal-build-environment:3 make -j4 -C src tests || exit -1
+      jamesmcclain/gdal-build-environment:4 make -j4 -C src tests || exit -1
 
 docker run -it --rm \
       -v $(pwd):/workdir \
       -e CC=gcc -e CXX=g++ \
       -e JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" \
-      jamesmcclain/gdal-build-environment:3 make -j4 -C src/experiments/thread pattern oversubscribe || exit -1
+      jamesmcclain/gdal-build-environment:4 make -j4 -C src/experiments/thread pattern oversubscribe || exit -1
 
 rm -f $(find | grep '\.o$')
 docker run -it --rm \
@@ -24,11 +25,11 @@ docker run -it --rm \
       -e CC=cc -e CXX=c++ \
       -e CFLAGS="-Wall -Werror -O0 -ggdb3" \
       -e JAVA_HOME="/macintosh/jdk8u202-b08/Contents/Home" \
-      -e GDALCFLAGS="-I/macintosh/gdal/2.4.0_1/include" \
+      -e GDALCFLAGS="-I/macintosh/gdal/2.4.2_3/include" \
       -e CXXFLAGS="-I/usr/osxcross/SDK/MacOSX10.10.sdk/usr/include/c++/v1"  \
       -e BOOST_ROOT="/usr/local/include/boost_1_69_0" \
-      -e LDFLAGS="-mmacosx-version-min=10.9 -L/macintosh/gdal/2.4.0_1/lib -lgdal -lstdc++ -lpthread" \
-      jamesmcclain/gdal-build-environment:3 make -j4 -C src libgdalwarp_bindings.dylib || exit -1
+      -e LDFLAGS="-mmacosx-version-min=10.9 -L/macintosh/gdal/2.4.2_3/lib -lgdal -lstdc++ -lpthread" \
+      jamesmcclain/gdal-build-environment:4 make -j4 -C src libgdalwarp_bindings.dylib || exit -1
 
 docker run -it --rm \
       -v $(pwd):/workdir \
@@ -36,9 +37,10 @@ docker run -it --rm \
       -e OS=win32 \
       -e CFLAGS="-Wall -Werror -Os -g" \
       -e JAVA_HOME="/windows/jdk8u202-b08" \
+      -e GDALCFLAGS="-I/usr/local/include" \
       -e BOOST_ROOT="/usr/local/include/boost_1_69_0" \
       -e LDFLAGS="-L/windows/gdal/lib -lgdal_i -lstdc++ -lpthread -lws2_32" \
-      jamesmcclain/gdal-build-environment:3 make -j4 -C src gdalwarp_bindings.dll || exit -1
+      jamesmcclain/gdal-build-environment:4 make -j4 -C src gdalwarp_bindings.dll || exit -1
 
 cp src/libgdalwarp_bindings.dylib src/main/java/resources/ || exit -1
 cp src/gdalwarp_bindings.dll src/main/java/resources/ || exit -1
