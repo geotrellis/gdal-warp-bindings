15aece9086dd3e7fa27a5c9d71898a29fe2fecd4
Grigory Pomadchin <gr.pomadchin@gmail.com>
2020-08-03 17:30:55 -0400

    Upd up to GDAL 3.2.0

--- scripts/publish-local.sh
+++ scripts/publish-local.sh
@@ -8,13 +8,13 @@ docker run -it --rm \
       -e CFLAGS="-Wall -Wno-sign-compare -Werror -O0 -ggdb3 -DSO_FINI -D_GNU_SOURCE" \
       -e BOOST_ROOT="/usr/local/include/boost_1_69_0" \
       -e JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" \
-      jamesmcclain/gdal-build-environment:4 make -j4 -C src tests || exit -1
+      daunnc/gdal-build-environment:5 make -j4 -C src tests || exit -1
 
 docker run -it --rm \
       -v $(pwd):/workdir \
       -e CC=gcc -e CXX=g++ \
       -e JAVA_HOME="/usr/lib/jvm/java-8-openjdk-amd64" \
-      jamesmcclain/gdal-build-environment:4 make -j4 -C src/experiments/thread pattern oversubscribe || exit -1
+      daunnc/gdal-build-environment:5 make -j4 -C src/experiments/thread pattern oversubscribe || exit -1
 
 rm -f $(find | grep '\.o$')
 docker run -it --rm \
@@ -25,11 +25,11 @@ docker run -it --rm \
       -e CC=cc -e CXX=c++ \
       -e CFLAGS="-Wall -Werror -O0 -ggdb3" \
       -e JAVA_HOME="/macintosh/jdk8u202-b08/Contents/Home" \
-      -e GDALCFLAGS="-I/macintosh/gdal/2.4.2_3/include" \
+      -e GDALCFLAGS="-I/macintosh/gdal/3.1.2/include" \
       -e CXXFLAGS="-I/usr/osxcross/SDK/MacOSX10.10.sdk/usr/include/c++/v1"  \
       -e BOOST_ROOT="/usr/local/include/boost_1_69_0" \
-      -e LDFLAGS="-mmacosx-version-min=10.9 -L/macintosh/gdal/2.4.2_3/lib -lgdal -lstdc++ -lpthread" \
-      jamesmcclain/gdal-build-environment:4 make -j4 -C src libgdalwarp_bindings.dylib || exit -1
+      -e LDFLAGS="-mmacosx-version-min=10.9 -L/macintosh/gdal/3.1.2/lib -lgdal -lstdc++ -lpthread" \
+      daunnc/gdal-build-environment:5 make -j4 -C src libgdalwarp_bindings.dylib || exit -1
 
 docker run -it --rm \
       -v $(pwd):/workdir \
@@ -40,7 +40,7 @@ docker run -it --rm \
       -e GDALCFLAGS="-I/usr/local/include" \
       -e BOOST_ROOT="/usr/local/include/boost_1_69_0" \
       -e LDFLAGS="-L/windows/gdal/lib -lgdal_i -static-libgcc -static-libstdc++ -Wl,-Bstatic -lstdc++ -lpthread -Wl,-Bdynamic -lws2_32" \
-      jamesmcclain/gdal-build-environment:4 make -j4 -C src gdalwarp_bindings.dll || exit -1
+      daunnc/gdal-build-environment:5 make -j4 -C src gdalwarp_bindings.dll || exit -1
 
 rm -f src/main/resources/*.so
 mv src/libgdalwarp_bindings.so src/main/resources/resources/
