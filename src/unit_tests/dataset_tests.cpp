682f3c4dca7823c6eb3854bf44fc8c9d1963407e
James W. McClain <jmcclain@azavea.com>
2021-05-14 22:11:32 -0400

    Make Minimal Accommodation for Older GDAL Versions

--- src/unit_tests/dataset_tests.cpp
+++ src/unit_tests/dataset_tests.cpp
@@ -116,7 +116,9 @@ BOOST_AUTO_TEST_CASE(get_offset)
     // - https://github.com/OSGeo/gdal/commit/69f25f253d141faf836c400676f9f94dd3f43707
     // 
     // BOOST_TEST(success != false);
+#if !defined(PRE_GDAL_31)
     BOOST_TEST(success == false);
+#endif
     success = false;
 
     ld.get_offset(locked_dataset::WARPED, 1, &offset, &success);
@@ -168,7 +170,9 @@ BOOST_AUTO_TEST_CASE(get_scale)
     // - https://github.com/OSGeo/gdal/commit/69f25f253d141faf836c400676f9f94dd3f43707
     // 
     // BOOST_TEST(success != false);
+#if !defined(PRE_GDAL_31)
     BOOST_TEST(success == false);
+#endif
     success = false;
 
     ld.get_scale(locked_dataset::WARPED, 1, &scale, &success);
